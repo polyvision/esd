264606e116b4a7a7a3033418272962542dc32441
Alexander Bierbrauer <github@polyvision.org>
2013-12-06 01:54:32 +0100

    added fake_start command tp esd

--- daemon.sh
+++ daemon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/emitos/esd
+./esd fake_start
 ./esd daemon
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <QCoreApplication>
 #include <QTextStream>
 
@@ -58,6 +59,16 @@ int main(int argc,char **argv){
 			default_stuff();
 			AppSettings::set_playback_bit_rate(atoi(argv[2]));
 		}
+		else if(strcmp(argv[1],"fake_start") == 0){
+			printf("fake start started");
+			default_stuff();
+			QCoreApplication a(argc, argv);
+			SoundEngine::instance()->initialize();
+			SoundEngine::instance()->start(); // starting the thread for the sound server
+			SoundEngine::instance()->start_recording();
+			sleep(5);
+			printf("fake start done");
+		}
 		else if(strcmp(argv[1],"daemon") == 0){
 			QCoreApplication a(argc, argv);
 			
@@ -80,7 +91,8 @@ int main(int argc,char **argv){
 			return QCoreApplication::exec();
 		}
 	}else{
-		printf("EMITOS sound daemon v1.0.1 - (c) 2013 by polyvision UG\n");
+		printf("EMITOS sound daemon v1.0.2 - (c) 2013 by polyvision UG\n");
+		printf("fake_start - fake start, initializes sound devices one time\n");
 		printf("list_sound_devices - listing sound devices\n");
 		printf("list_recording_devices - listing recording sound devices\n");
 		printf("set_recording_device NUM - set recording devices\n");
